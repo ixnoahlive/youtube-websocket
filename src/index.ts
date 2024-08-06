1559683d87d3037512696d87cfbd46faae6c1042
ixNoah <im.a.boiled.egg@gmail.com>
2024-08-07 00:41:51 +0100

    Make websocket routing inline to improve readability

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,6 @@ import { getChannel } from './routes/channel';
 import { getStream } from './routes/stream';
 
 new Elysia()
-    .ws('/c/:id', {
-        open: getChannel
-    })
-    .ws('/s/:id', {
-        open: getStream
-    })
+    .ws('/c/:id', { open: getChannel })
+    .ws('/s/:id', { open: getStream  })
     .listen(process.env.PORT || 9905)
